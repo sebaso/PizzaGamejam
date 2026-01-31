6df54e9b1ce7bf350eadc0964cb38b7aa9e8d339
Jose Arenas Venegas <pepearenasvenegas@gmail.com>
2026-01-31 20:35:22 +0100

    menu

---
+++ Assets/Scripts/MENU/PauseMenu.cs
@@ -0,0 +1,64 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    [Header("UI")]
+    public GameObject panelPausa;
+    
+    private bool estaPausado = false;
+
+    void Start()
+    {
+        // Asegurar que el menú está oculto al inicio
+        if (panelPausa != null)
+        {
+            panelPausa.SetActive(false);
+        }
+    }
+
+    void Update()
+    {
+        // Detectar tecla ESC para pausar/despausar
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (estaPausado)
+            {
+                Reanudar();
+            }
+            else
+            {
+                Pausar();
+            }
+        }
+    }
+
+    public void Pausar()
+    {
+        estaPausado = true;
+        panelPausa.SetActive(true);
+        Time.timeScale = 0f; // Congela el juego
+    }
+
+    public void Reanudar()
+    {
+        estaPausado = false;
+        panelPausa.SetActive(false);
+        Time.timeScale = 1f; // Reanuda el juego
+    }
+
+    public void VolverAlMenu()
+    {
+        Time.timeScale = 1f; // Asegurar que el tiempo vuelva a la normalidad
+        SceneManager.LoadScene("MainMenu"); // Cambia "MainMenu" por el nombre de tu escena de menú principal
+    }
+
+    public void Salir()
+    {
+        #if UNITY_EDITOR
+            UnityEditor.EditorApplication.isPlaying = false;
+        #else
+            Application.Quit();
+        #endif
+    }
+}
